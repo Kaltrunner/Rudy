c853dcf79af50349c9160f60b384a05f18503298
Erik Russell <erikrussells@gmail.com>
2023-04-29 19:41:40 -0400

    again

--- render-build.sh
+++ render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 bundle install
 # bundle exec rails assets:precompile
 # bundle exec rails assets:clean
-bundle exec rails db:migrate
+# bundle exec rails db:migrate
 
 #if you have seeds to run add:
 # bundle exec rails db:seed
